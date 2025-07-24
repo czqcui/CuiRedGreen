65240b09ac29a6c264916f8ef447aaf03389d179
czqcui <czqcui@cn.ibm.com>
2025-07-24 23:04:23 +0800

    init commit

---
+++ src/main/java/com/test/red/green/CuiRedGreen/CuiRedGreenApplication.java
@@ -0,0 +1,13 @@
+package com.test.red.green.CuiRedGreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuiRedGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuiRedGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/red/green/CuiRedGreen/CuiRedGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.red.green.CuiRedGreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuiRedGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
